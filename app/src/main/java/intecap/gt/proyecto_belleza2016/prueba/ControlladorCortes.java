c7123e59aaddcb8d7cf22a6adc753f46a1a00ac8
joseefgar <jfgarciap18@gmail.com>
2016-10-03 16:15:16 -0600

    cortes y peinados

--- app/src/main/java/intecap/gt/proyecto_belleza2016/prueba/ControlladorCortes.java
+++ app/src/main/java/intecap/gt/proyecto_belleza2016/prueba/ControlladorCortes.java
@@ -67,7 +67,22 @@ public class ControlladorCortes {
                 "te ayudará a separar el cabello en secciones iguales para hacer el corte. Las bandas mantendrán las secciones de " +
                 "cabello separadas mientras cortas."  ,"5coJa7lrRQw"));
         agregarItem(new Cortes(generarId(),
-                "Niños", R.drawable.peinado_ninos, "" ,"5coJa7lrRQw"));
+                "Niños", R.drawable.peinado_ninos, "Primero debemos reunir los materiales adecuados: 1 Consigue un " +
+                "buen par de tijeras de peluquero. Si decides cortarte el cabello por tu cuenta, debes buscar tijeras de buena " +
+                "calidad especialmente diseñadas para cortar cabello. Puedes encontrarlas en línea o en las tiendas de suministros " +
+                "para salones de belleza. Usar tijeras de buena calidad hará que sea más fácil cortarte el cabello en casa.Debes" +
+                " buscar tijeras de peluquero que tengan una cuchilla de acero inoxidable y una cuchilla afilada. No utilices " +
+                "tijeras de cocina, tijeras para uso doméstico o tijeras de jardinería, ya que podrían arrancarte o dañarte el " +
+                "cabello." +
+                "2 Compra una maquinilla para cortar cabello y una rasuradora para cabello. Puedes encontrarlas en línea o en una " +
+                "tienda de suministros para salones de belleza. Consigue maquinillas eléctricas de buena calidad, ya que vas a usar " +
+                "estas herramientas para hacer el corte bob.Una rasuradora para cabello también será útil para hacer este corte, ya " +
+                "que te permitirá dar forma y recortar el cabello sin tener que hacer ningún corte mayor. Asegúrate de que la " +
+                "rasuradora esté afilada, puesto que una cuchilla sin filo hará que sea más difícil de usar." +
+                "3 Consigue un peine y bandas elásticas para cabello. También necesitarás un peine y tres bandas elásticas. Puedes " +
+                "encontrar estos productos en línea o en el pasillo de artículos de belleza de cualquier farmacia local. El peine " +
+                "te ayudará a separar el cabello en secciones iguales para hacer el corte. Las bandas mantendrán las secciones de " +
+                "cabello separadas mientras cortas."  ,"5coJa7lrRQw"));
     }
 
     private static void agregarItem(Cortes item) {
